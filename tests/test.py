a10a6141b5c9ce8707cf039d265a911adfb9e27e
Kit Sutliff <38538497+KitSutliff@users.noreply.github.com>
2024-07-23 12:11:36 -0700

    includes failing test to troubleshoot

---
+++ tests/test.py
@@ -0,0 +1,7 @@
+import pytest
+from app import app
+
+def test_say_hello_world(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b"Hello, World!"
