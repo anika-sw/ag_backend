34fbb31ab6a81f401fccb4ab63712f8a05314928
Kit Sutliff <38538497+KitSutliff@users.noreply.github.com>
2024-07-23 10:43:06 -0700

    first route test returns hello world

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from app.routes import hello_world_bp
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__)
+
+    # Register Blueprints here
+    app.register_blueprint(hello_world_bp)
 
     return app
