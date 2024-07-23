3d28ca3a1a33f9e27c428b157044293742fbe6f5
Anika Stephen Wilbur <anika.m.stephen@gmail>
2024-07-23 12:55:54 -0700

    First test running

--- tests/_test.py
+++ tests/_test.py
@@ -1,6 +1,3 @@
-import pytest
-
-@pytest.fixture
 def test_say_hello_world(client):
     response = client.get('/')
     assert response.status_code == 200
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from app import create_app
 
 @pytest.fixture
 def client():
+    app = create_app()
     with app.test_client() as client:
         yield client
 
