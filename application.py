7e77c348cd43e162e3faeab7d239595097f483ed
sunnybysunny <1sunnymuniz@gmail.com>
2024-07-26 09:57:50 -0600

    changed application path

--- application.py
+++ application.py
@@ -1,10 +1,6 @@
 from flask import Blueprint
 
 hello_world_bp = Blueprint("hello_world_bp", __name__)
-application = Flask(__name__)
-
-# Register Blueprints here
-application.register_blueprint(hello_world_bp)
 
 @hello_world_bp.get("/")
 def say_hello_world():
