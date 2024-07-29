c4329e04c2d10b96f5f6ea0a63c8cfac4651bedc
Anika Stephen Wilbur <anika.m.stephen@gmail>
2024-07-29 09:19:43 -0700

    Uncommented code from three files to get backend running locally

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
-# from flask import Flask
-# from app.routes import hello_world_bp
+from flask import Flask
+from app.routes import hello_world_bp
 
-# # we could try taking application out of function 
+def create_app():
+    app = Flask(__name__)
 
-# def create_app():
-#     application = Flask(__name__)
+    # Register Blueprints here
+    app.register_blueprint(hello_world_bp)
 
-#     # Register Blueprints here
-#     application.register_blueprint(hello_world_bp)
-
-#     return application
+    return app
--- tests/_test.py
+++ tests/_test.py
@@ -1,4 +1,4 @@
-# def test_say_hello_world(client):
-#     response = client.get('/')
-#     assert response.status_code == 200
-#     assert response.data == b"Hello, World!"
+def test_say_hello_world(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b"Hello, World!"
