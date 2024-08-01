74e241ee1aa6e4ff23eca1cc674e4e942a76bb54
Kit Sutliff <38538497+KitSutliff@users.noreply.github.com>
2024-08-01 09:43:29 -0700

    cleans up testing

--- tests/_test.py
+++ tests/_test.py
@@ -15,9 +15,6 @@ import os
 
 
 @pytest.fixture
-    # def app_context():
-    # with create_app() :  # Replace 'your_app' with your Flask app object
-    #     yield
 def app_context():
     app = create_app() # Initialize your Flask app 
     with app.app_context(): 
@@ -48,10 +45,8 @@ def test_get_user_inputs_missing_parameter(app_context):
 def test_get_user_inputs_not_in_list(app_context):
     mock_request = MagicMock()
     mock_request.get_json.return_value = {'genre': ["country"], 'mood': ["angry"], 'tempo': 'medium'}
-    print(mock_request.get_json())
     
     response = get_user_inputs(mock_request)
-    print(response)
 
     assert response[0] == "The 'tempo' parameter is required and must be a list containing at least element."
     assert response[1] == 400
@@ -105,7 +100,8 @@ def test_generate_song_from_api_is_called_once(client):
         # Assert the response from the Flask endpoint
         assert response.status_code == 200
         assert response.get_json() == 'mocked song data'
-
+        # Ensures the api has been called only once
+        assert mock_request.call_count == 1
 
         
 
