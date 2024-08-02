d48689e3bdc0ea3711072870e031456b6781dbb9
Anika Stephen Wilbur <anika.m.stephen@gmail>
2024-08-02 16:18:22 -0700

    Revert "Update CORS in __init__.py to accept all origins"
    This reverts commit d9edbb2b3008993cfec92160b65ec5cb530e837b.

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from app.routes import song_bp
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "*"}})
+    CORS(app)
 
     # Register Blueprints here
     # app.register_blueprint(hello_world_bp) # uncomment for eb
